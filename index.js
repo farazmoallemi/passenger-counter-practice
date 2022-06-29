9ffb16992ee10e6ee6c05f469c7b65af437aa3d0
farazmoallemi <moallemifaraz@gmail.com>
2022-06-28 22:49:00 -0700

    creating the passenger subway application with incrementer

--- index.js
+++ index.js
@@ -1,7 +1,16 @@
-// document.getElementById("count-el").innerText = 5
+// bring neccesary elements from HTML file
+const elementCount = document.getElementById("count-el");
 
 // create a varibale to store the count of people entering the subway
-
 let subwayPassengerCount = 0;
+// listen for when the user clicks on the incrment button
+const increment = () => {
+  subwayPassengerCount += 1;
+  elementCount.innerText = subwayPassengerCount;
+}
 
-subwayPassengerCount = 5;
+
+
+// increment the count variable when the button is clicked
+
+// change the value of the count in the html to match the new count
