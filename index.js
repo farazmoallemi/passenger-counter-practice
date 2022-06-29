4aaf608fd97a66510b4078535d753688e4af61d2
farazmoallemi <moallemifaraz@gmail.com>
2022-06-29 14:46:28 -0700

    adding the save button and finishing up the subway passenger count application without the CSS

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
-// bring neccesary elements from HTML file
-const elementCount = document.getElementById("count-el");
-
-// create a varibale to store the count of people entering the subway
+// bring the neccessary HTML element(s) into the JavaScript file
+let elementCount = document.getElementById('count-el');
+let previousEntries = document.getElementById('previous-entries')
+// create a variable to initialize the count of passengers in the subway
 let subwayPassengerCount = 0;
-// listen for when the user clicks on the incrment button
-const increment = () => {
+// create a function to increment the count of passengers
+const incrementPassengerCount = () => {
+  // increment the count of the passengers by 1 when this button is clicked
   subwayPassengerCount += 1;
-  elementCount.innerText = subwayPassengerCount;
+  // reassign the count of passengers to be the new count of passengers
+  elementCount.textContent = subwayPassengerCount;
 }
-
-
-
-// increment the count variable when the button is clicked
-
-// change the value of the count in the html to match the new count
+// create a button that saves the current passenger count
+const saveCurrentPassengerCount = () => {
+  // add the subway passenger count with a dash to the
+  // previous entries label when save is clicked
+  previousEntries.textContent += `${subwayPassengerCount} - `
+  // when save is pushed, change the subway passenger 
+  // count to 0 as well as the label
+  subwayPassengerCount = 0;
+  elementCount.textContent = subwayPassengerCount;
+}
