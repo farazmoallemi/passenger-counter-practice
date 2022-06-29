820b3d5ee274d2e1fb0950b3b296bd9862ba3f87
farazmoallemi <moallemifaraz@gmail.com>
2022-06-28 22:22:18 -0700

    First commit and initial creation of files

---
+++ index.js
@@ -0,0 +1,7 @@
+// document.getElementById("count-el").innerText = 5
+
+// create a varibale to store the count of people entering the subway
+
+let subwayPassengerCount = 0;
+
+subwayPassengerCount = 5;
